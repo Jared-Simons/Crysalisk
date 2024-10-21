1bf6cd8d01d7ce9b6007d97fbbe331ae3cc8fde0
Jared-Simons <JRod.Simons@gmail.com>
2024-10-20 23:06:32 -0400

    Added makefile

--- build-all.bat
+++ build-all.bat
@@ -1,5 +1,7 @@
+@echo off
+
 REM Build Engine
-CALL clang engine\src\core\engine.c engine\src\core\logging.c engine\src\platform\platform_win32.c -Iengine\src\ -g -shared -fdeclspec -o bin\engine.dll -luser32.lib -DPLAT_WIN32 -DDLL_EXPORT -D_CRT_SECURE_NO_WARNINGS
+CALL make ASSEMBLY=engine EXTENSION=dll
 
 REM Build Testapp
-CALL clang testapp\src\main.c -Iengine\src\ -g -o bin\testapp.exe -lbin\engine.lib
+CALL make ASSEMBLY=testapp EXTENSION=exe ADDL_INCLUDE_DIRS=-Iengine\src ADDL_LINKER_FLAGS=-lbin\engine.lib
